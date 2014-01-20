7ffe84fed2f0ef12ee3c2a5f65a6a9f67dc2016d
Taylor Savage <taylor@taylorsavage.com>
2014-01-20 14:10:44 -0800

    initial commit

---
+++ canvas.js
@@ -0,0 +1,41 @@
+var control = require('control'),
+	task = control.task;
+
+var config = require('./config.json');
+
+function configure(hostname, address, sshPort) {
+	var controller = Object.create(control.controller);
+
+	controller.user = 'pi';
+	controller.sshOptions = ['-p ' + sshPort];
+	controller.scpOptions = ['-P ' + sshPort, '-r'];
+	controller.address = address;
+	controller.localDir = "./" + hostname;
+	controller.remoteDir = "/home/pi/easel";
+	controller.hostname = hostname;
+	controller.config = config.machines[hostname];
+
+	return [controller];
+}
+
+task('vincent', 'Config for vincent', function() {
+	return configure('vincent', 'taylorsavage.com', '2219');
+});
+
+task('pablo', 'Config for pablo', function() {
+	return configure('pablo', 'taylorsavage.com', '2221');
+});
+
+task('pablo-eth', 'Config for pablo over ethernet', function() {
+	return configure('pablo', 'taylorsavage.com', '2220');
+});
+
+task('deploy', 'Deploy a machine', function(controller) {
+	controller.ssh("cd /home/pi/canvas/canvas-client && git pull origin master && npm install", function() {
+		controller.scp("./config." + controller.hostname + ".json", "/home/pi/canvas/config.json", function() {
+			controller.ssh("/etc/init.d/nodejs.sh restart");
+		});
+	});
+});
+
+control.begin();
