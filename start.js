145ff75efeaded21b199ff2f15d30153a2d7c1a8
Taylor Savage <taylor@taylorsavage.com>
2014-01-20 15:11:01 -0800

    changed start

--- start.js
+++ start.js
@@ -1,17 +1,18 @@
 var child_process = require('child_process');
 var config = require('./config.json');
 var fs = require('fs');
+var path = require('path');
 
 var childProcesses = [];
 
 if (config.server) {
 	console.log("starting server");
-	childProcesses.push(child_process.fork('../canvas-server/app'));
+	childProcesses.push(child_process.fork(__dirname + "/../canvas-server/app"));
 }
 
 for (var index in config.clients) {
 	var client = config.clients[index];
 	console.log("starting client",client);
 	fs.writeFile("/tmp/" + client.slug + ".config.json", JSON.stringify(client));
-	childProcesses.push(child_process.fork('../canvas-client/app', ['/tmp/' + client.slug + '.config.json']));
+	childProcesses.push(child_process.fork(__dirname + '/../canvas-client/app', ['/tmp/' + client.slug + '.config.json']));
 }
