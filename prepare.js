bb5540aff868eb00510efbdab1639669aa021195
Taylor Savage <taylor@taylorsavage.com>
2014-01-28 06:34:41 -0800

    switched back to pm2

--- prepare.js
+++ prepare.js
@@ -5,11 +5,18 @@ var path = require('path');
 var sleep = require('sleep');
 var forever = require('forever-monitor');
 
-var startString = "";
+var pm2Config =[];
 for (var index in config.clients) {
 	var client = config.clients[index];
 	fs.writeFileSync(__dirname + "/config/" + client.slug + ".config.json", JSON.stringify(client));
-	startString += "forever start /home/pi/canvas/canvas-client/app.js /home/pi/canvas/canvas-deploy/config/" + client.slug + ".config.json -o /home/pi/canvas/logs/" + client.slug + "_out.log -e /home/pi/canvas/logs/" + client.slug + "_err.log;  ";
+	
+	pm2Config.push({
+		"name": client.slug,
+		"script": "/home/pi/canvas/canvas-client/app.js",
+		"args": "['/home/pi/canvas/canvas-deploy/config/" + client.slug + ".config.json']",
+		"error_file": "/home/pi/canvas/logs/" + client.slug + ".err.log",
+		"out_file": "/home/pi/canvas/logs/" + client.slug + ".out.log"
+	});
 }
 
-fs.writeFileSync(__dirname + "/start.sh", startString);
+fs.writeFileSync(__dirname + "/pm2Config.json", JSON.stringify(pm2Config));
