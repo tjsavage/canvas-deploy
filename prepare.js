7371d139db5f9af8fdb36390e7fea238425cfd9e
Taylor Savage <taylor@taylorsavage.com>
2014-03-13 15:44:48 -0700

    changed deploy script to allow for different start scripts

--- prepare.js
+++ prepare.js
@@ -10,14 +10,15 @@ for (var index in config.clients) {
 	var client = config.clients[index];
 	fs.writeFileSync(__dirname + "/config/" + client.slug + ".config.json", JSON.stringify(client));
 	
-	pm2Config.push({
+	var pm2Object = {
 		"name": client.slug,
-		"script": "/home/pi/canvas/canvas-client/app.js",
+		"script": client.script || "/home/pi/canvas/canvas-client/app.js",
 		"args": "['/home/pi/canvas/canvas-deploy/config/" + client.slug + ".config.json']",
 		"error_file": "/home/pi/canvas/logs/" + client.slug + ".err.log",
 		"out_file": "/home/pi/canvas/logs/" + client.slug + ".out.log",
 		"user": "root"
-	});
+	};
+	pm2Config.push(pm2Object);
 }
 
 fs.writeFileSync(__dirname + "/pm2Config.json", JSON.stringify(pm2Config));
